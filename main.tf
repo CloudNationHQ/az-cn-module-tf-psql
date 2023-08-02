0880bb5a3b8c2c8a5ab21ad6f7efce5283c1cf38
Eddy Vera <eddy.ver@cloudnation.nl>
2023-08-02 16:50:35 +0200

    added the last missing properties and flex db's

--- main.tf
+++ main.tf
@@ -16,10 +16,18 @@ resource "azurerm_postgresql_flexible_server" "postgresql" {
   storage_mb                   = try(var.postgresql.storage_mb, 32768)
   backup_retention_days        = try(var.postgresql.backup_retention_days, null )
   geo_redundant_backup_enabled = try(var.postgresql.geo_redundant_backup_enabled, false)
+  zone                         = try(var.postgresql.zone, null)
 
   create_mode                  = try(var.postgresql.create_mode, "Default")
   administrator_login          = try(var.postgresql.create_mode, "Default") == "Default" ? "pgsql_${var.workload}_${var.environment}_admin" : null
   administrator_password       = random_password.pgsql_admin_password.result
+
+  point_in_time_restore_time_in_utc = try(var.postgresql.create_mode, null) == "PointInTimeRestore" ? var.postgresql.restore_time_utc : null
+  replication_role = try(var.postgresql.create_mode, null) == "Replica" ? "None" : null
+  source_server_id = try(var.postgresql.create_mode, null) == "PointInTimeRestore" || try(var.postgresql.create_mode, null) == "Replica" ? var.postgresql.source_server_id : null
+
+  delegated_subnet_id =  try(azurerm_subnet.postgresql["subnet"].id, null)
+  private_dns_zone_id =  try(azurerm_private_dns_zone.postgresql["dns"].id, null)
  
     dynamic "identity"{
     for_each = try(var.postgresql.identity.user_assigned_identity, null) == true ? [1]: []
@@ -28,7 +36,7 @@ resource "azurerm_postgresql_flexible_server" "postgresql" {
     type = try(var.postgresql.identity.user_assigned_identity, null) == true ? "UserAssigned" : null
     identity_ids = concat([azurerm_user_assigned_identity.primary_identity["identity"].id], try(var.postgresql.identity.other_identity_ids, []))
     }
-    }
+  }
 
     dynamic "customer_managed_key" {
     for_each = try(var.postgresql.cmk, null) != null ? var.postgresql.cmk : {}
@@ -44,7 +52,6 @@ resource "azurerm_postgresql_flexible_server" "postgresql" {
     dynamic "authentication" {
     for_each = try(var.postgresql.auth, null) != null ? [1] : []
 
-
     content {
     active_directory_auth_enabled   = try(var.postgresql.auth.ad_auth_enabled, true)
     password_auth_enabled           = try(var.postgresql.auth.pw_auth_enabled, true)
@@ -52,7 +59,14 @@ resource "azurerm_postgresql_flexible_server" "postgresql" {
     }
   }
 
-    
+    dynamic "high_availability" {
+    for_each = try(var.postgresql.high_availability, null) != null ? [1] : []
+
+    content {
+    mode   = try(var.postgresql.high_availability.mode, "SameZone")
+    standby_availability_zone = try(var.postgresql.high_availability.standby_availability_zone, null)
+    }
+  }
 
     dynamic "maintenance_window" {
     for_each = try(var.postgresql.maintenance_window, null) != null ? var.postgresql.maintenance_window : []
@@ -62,7 +76,7 @@ resource "azurerm_postgresql_flexible_server" "postgresql" {
     start_hour      = try(var.postgresql.maintenance_window.start_hour, null)
     start_minute    = try(var.postgresql.maintenance_window.start_minute, null)      
     }
-    }
+  }
 
   tags = try(var.tags, null)
 
@@ -85,25 +99,24 @@ resource "azurerm_postgresql_flexible_server_active_directory_administrator" "po
   resource_group_name = var.postgresql.resource_group
   tenant_id           = data.azurerm_client_config.current.tenant_id
   object_id           = try(var.postgresql.auth.object_id, data.azurerm_client_config.current.object_id)
-  principal_type      = "ServicePrincipal"
-#   principal_type = "User" # for local testing
+#   principal_type      = "ServicePrincipal"
+  principal_type = "User" # for local testing
   principal_name      = try(var.postgresql.auth.principal_name, data.azuread_user.current.display_name)
 }
 
-# data "azuread_user" "current" {  # for local testing
-#   object_id = data.azurerm_client_config.current.object_id
-# }
-
-data "azuread_service_principal" "current" {
+data "azuread_user" "current" {  # for local testing
   object_id = data.azurerm_client_config.current.object_id
 }
 
-# resource "azurerm_postgresql_database" "main" {
-#   for_each = local.databases_map
-
-#   name                = each.key
-#   resource_group_name = var.postgresql.resource_group
-#   server_name         = azurerm_postgresql_server.main.name
-#   charset             = each.value.charset
-#   collation           = each.value.collation
+# data "azuread_service_principal" "current" {
+#   object_id = data.azurerm_client_config.current.object_id
 # }
+
+resource "azurerm_postgresql_flexible_server_database" "database" {
+  for_each = try({for database in var.postgresql.databases: database.name => database}, {})
+
+  name                = each.key
+  server_id           = azurerm_postgresql_flexible_server.postgresql.id
+  charset             = try(each.value.charset, null)
+  collation           = try(each.value.collation, null)
+}
