8249246c37eb2431cef9fcbdc2b30706493b1248
eddy-vera <122470243+eddy-vera@users.noreply.github.com>
2023-08-22 20:33:45 +0200

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -3,34 +3,22 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.16"
+      version = "~> 3.70.0"
     }
     postgresql = {
       source  = "cyrilgdn/postgresql"
-      version = "~> 1.14.0"
+      version = "~> 1.20.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.1.0"
+      version = "~> 3.5.1"
     }
     azuread = {
       source = "hashicorp/azuread"
-      version = "~> 2.15.0"
+      version = "~> 2.41.0"
     }
   }
 }
 
 data "azurerm_client_config" "current" {
 }
-
-# provider "postgresql" {
-#   host             = azurerm_postgresql_server.main.fqdn
-#   port             = 5432
-#   username         = "${azurerm_postgresql_server.main.administrator_login}@${azurerm_postgresql_server.main.name}"
-#   password         = data.azurerm_key_vault_secret.pg_admin.value
-#   sslmode          = "require"
-#   superuser        = false
-#   connect_timeout  = 15
-#   expected_version = var.server_version
-#   max_connections  = 1
-# }
