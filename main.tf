1c04ffdc484443977db36f2a4474832c47674927
eddyvera <eddy.ver@cloudnation.nl>
2023-12-09 11:14:47 +0100

    fix delegated+rep-role+cmk and support for 2nd MI

--- main.tf
+++ main.tf
@@ -22,30 +22,30 @@ resource "azurerm_postgresql_flexible_server" "postgresql" {
   administrator_login    = try(var.postgresql.create_mode, "Default") == "Default" || try(var.postgresql.enabled.pw_auth, null) == true ? "${replace(var.postgresql.name, "-", "_")}_admin" : null
   administrator_password = try(var.postgresql.create_mode, "Default") == "Default" || try(var.postgresql.enabled.pw_auth, null) == true ? try(var.postgresql.admin_password, random_password.psql_admin_password.result) : null
 
-  delegated_subnet_id = try(var.postgresql.network.subnet_id, null)
+  delegated_subnet_id = try(var.postgresql.network.delegated_subnet_id, null)
   private_dns_zone_id = try(var.postgresql.network.private_dns_zone_id, null)
 
   source_server_id                  = try(var.postgresql.create_mode, null) == "PointInTimeRestore" || try(var.postgresql.create_mode, null) == "Replica" ? var.postgresql.source_server_id : null
   point_in_time_restore_time_in_utc = try(var.postgresql.create_mode, null) == "PointInTimeRestore" ? var.postgresql.restore_time_utc : null
-  replication_role                  = try(var.postgresql.create_mode, null) == "Replica" ? "None" : null
+  replication_role                  = try(var.postgresql.replication_role, null)
 
   dynamic "identity" {
     for_each = try(var.postgresql.identity.user_assigned_identity, null) == true ? [1] : []
 
     content {
       type         = try(var.postgresql.identity.user_assigned_identity, null) == true ? "UserAssigned" : null
-      identity_ids = concat([azurerm_user_assigned_identity.primary_identity["identity"].id], try(var.postgresql.identity.other_identity_ids, []))
+      identity_ids = concat([azurerm_user_assigned_identity.primary_identity["identity"].id], [azurerm_user_assigned_identity.backup_identity["identity"].id])
     }
   }
 
   dynamic "customer_managed_key" {
-    for_each = try(var.postgresql.cmk, null) != null ? var.postgresql.cmk : {}
+    for_each = try(var.postgresql.cmk, null) != null ? [1] : []
 
     content {
       key_vault_key_id                     = try(var.postgresql.cmk.key_vault_key_id, null)
-      primary_user_assigned_identity_id    = azurerm_user_assigned_identity.primary_identity["identity"].id
-      geo_backup_key_vault_key_id          = try(var.postgresql.cmk.geo_backup_key_vault_key_id, null)
-      geo_backup_user_assigned_identity_id = try(var.postgresql.cmk.geo_backup_user_assigned_identity_id, null)
+      primary_user_assigned_identity_id    = try(azurerm_user_assigned_identity.primary_identity["identity"].id, null)
+      geo_backup_key_vault_key_id          = try(var.postgresql.cmk.key_vault_backup_key_id, null)
+      geo_backup_user_assigned_identity_id = try(azurerm_user_assigned_identity.backup_identity["identity"].id, null)
     }
   }
 
@@ -84,6 +84,7 @@ resource "azurerm_postgresql_flexible_server" "postgresql" {
     ignore_changes = [zone, high_availability.0.standby_availability_zone]
   }
 
+  depends_on = [azurerm_role_assignment.primary_identity, azurerm_role_assignment.backup_identity]
 }
 
 resource "azurerm_user_assigned_identity" "primary_identity" {
@@ -94,6 +95,30 @@ resource "azurerm_user_assigned_identity" "primary_identity" {
   resource_group_name = var.postgresql.resource_group
 }
 
+resource "azurerm_role_assignment" "primary_identity" {
+  for_each = try(var.postgresql.identity.user_assigned_identity, null) == true ? { "identity" = {} } : {}
+
+  scope                = var.postgresql.key_vault_id
+  role_definition_name = "Key Vault Crypto Officer"
+  principal_id         = azurerm_user_assigned_identity.primary_identity["identity"].principal_id
+}
+
+resource "azurerm_user_assigned_identity" "backup_identity" {
+  for_each = try(var.postgresql.identity.user_assigned_backup_identity, null) == true ? { "identity" = {} } : {}
+
+  location            = var.postgresql.location
+  name                = "${var.naming.user_assigned_identity}-${var.postgresql.name}-backup"
+  resource_group_name = var.postgresql.resource_group
+}
+
+resource "azurerm_role_assignment" "backup_identity" {
+  for_each = try(var.postgresql.identity.user_assigned_backup_identity, null) == true ? { "identity" = {} } : {}
+
+  scope                = var.postgresql.key_vault_backup_id
+  role_definition_name = "Key Vault Crypto Officer"
+  principal_id         = azurerm_user_assigned_identity.backup_identity["identity"].principal_id
+}
+
 resource "azurerm_postgresql_flexible_server_active_directory_administrator" "postgresql" {
   for_each = try(var.postgresql.enabled.ad_auth, null) == true ? { "ad_auth" = {} } : {}
 
@@ -101,7 +126,7 @@ resource "azurerm_postgresql_flexible_server_active_directory_administrator" "po
   resource_group_name = var.postgresql.resource_group
   tenant_id           = data.azurerm_client_config.current.tenant_id
   object_id           = try(var.postgresql.ad_admin.object_id, data.azurerm_client_config.current.object_id)
-  principal_type      = "ServicePrincipal"
+  principal_type      = try(var.postgresql.ad_admin.principal_type, "ServicePrincipal")
   principal_name      = try(var.postgresql.ad_admin.principal_name, data.azuread_service_principal.current.display_name)
 }
 
